28f5dad6eeba52e04ed94c4d0b193587702e7ef6
gisellebonetti <giselle.bonetti03@gmail.com>
2022-07-23 15:29:33 -0300

    tabuada - code girls

---
+++ tabuada.java
@@ -0,0 +1,19 @@
+
+import java.util.Scanner;
+
+
+public class Tabuada {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner (System.in);
+                
+        System.out.println("tabuada: ");
+        int tabuada = scan.nextInt ();
+    
+        System.out.println ("tabuada de " + tabuada);
+        for(int i = 1 ; i <= 10 ; i++) {
+    
+        System.out.println(tabuada + " X " + i + " = " + (tabuada*i));
+    }
+    
+}
+    }
